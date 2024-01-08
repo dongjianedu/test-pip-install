0f119f0dac7d16b30bf0011fc1cd73c39d5c1e81
deejac <deejac@qq.com>
2024-01-08 12:21:52 +0800

    sd api v2.1.1   test mllib

--- src/start-gpu.sh
+++ src/start-gpu.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
 echo "test gpu 2.1.0"
+
+echo "---------------env------------------"
+env
+echo "---------------env------------------"
+
+
+echo "---------------libnvidia-ml------------------"
+ls /usr/lib/x86_64-linux-gnu/libnvidia-ml.so.1
+echo "---------------libnvidia-ml------------------"
+
 nvidia-smi
